8335fffd441c3c59367a80443f4c7b66ef9e1fc9
Vincent Kenbeek <vkenbeek@gmail.com>
2023-05-30 12:50:04 +0200

    Fixed comment and simplified split_50_50

--- src/lock_free_programs.h
+++ src/lock_free_programs.h
@@ -21,30 +21,7 @@ public:
      * Each producer only produces 1 item
      */
     benchmark_result split_50_50_single(int threads) {
-        benchmark_result results;
-        if (threads % 2 != 0) threads--; // make sure there is an even amount of threads
-        if (threads <= 0) return results;
-
-        LockFreeBag bag(threads);
-        omp_set_num_threads(threads);
-        float t = omp_get_wtime();
-        #pragma omp parallel for
-        for (int i = 0; i < threads; i++) {
-            if (i % 2 == 0) {
-                bag.Add(i, i);
-            } else {
-                bag.TryRemoveAny(i);
-            }
-        }
-        results.time = omp_get_wtime() - t;
-
-        for (int i = 0; i < threads; i++) {
-            std::cout << "Thread " << i << " counters:" << std::endl;
-            bag.GetCounters(i).print();
-            results.add_results(bag.GetCounters(i));
-        }
-
-        return results;
+        return split_50_50(threads, 1);
     }
 
     /**
@@ -103,7 +80,7 @@ public:
 
     /**
      * One of the threads will be designated as consumer and all the others will be producers
-     * The producers will produce @p elements meaning there will in total be @p elements * (@p threads - 1) elements.
+     * The producers will produce @p elements meaning there will in total be @p elements * ( @p threads - 1 ) elements.
      * Once all these elements have been consumed the benchmark is over
      * @param threads   The number of threads (must be >= 2)
      * @param elements  The number of elements to be produced by each producer 
@@ -144,7 +121,8 @@ public:
 
     /**
      * 50% of threads will be produces and 50% will be consumers
-     * Each producer only produces 1 item
+     * Each producer will produce @p elements
+     * The benchmark is over once @p elements * ( @p threads / 2 ) elements have been consumed
      */
     benchmark_result split_50_50(int threads, int elements) {
         benchmark_result results;
